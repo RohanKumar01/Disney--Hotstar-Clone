9505894a3763ea856672993a5648696af5fbcf3e
RohanKumar01 <rohankumar25601@gmail.com>
2022-11-07 23:14:42 +0530

    login form and bug fixes

--- data.js
+++ data.js
@@ -12,12 +12,12 @@ let movies = [
       image: "images/slider 1.PNG",
     },
     {
-      name: "wanda vision",
+      name: "Thor - Love and thunder",
       des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
       image: "images/slider 3.PNG",
     },
     {
-      name: "raya and the last dragon",
+      name: "Moon knight",
       des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
       image: "images/slider 4.PNG",
     },
